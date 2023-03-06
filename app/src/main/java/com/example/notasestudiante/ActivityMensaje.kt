914efafb9039230243a97f5e524a179b7db033d3
minjibyung <minjibyung@gmail.com>
2023-03-06 17:42:28 -0500

    subiendo archivos

---
+++ app/src/main/java/com/example/notasestudiante/ActivityMensaje.kt
@@ -0,0 +1,11 @@
+package com.example.notasestudiante
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityMensaje : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mensaje)
+    }
+}
