b0aa21a6636ed1b75d5fae5fe14815b06d2c6d5f
minjibyung <minjibyung@gmail.com>
2023-03-08 17:42:55 -0500

    Modificando archivos

--- app/src/main/java/com/example/notasestudiante/ActivityMensaje.kt
+++ app/src/main/java/com/example/notasestudiante/ActivityMensaje.kt
@@ -2,10 +2,24 @@ package com.example.notasestudiante
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ActivityMensaje : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mensaje)
+
+        val campomensaje=findViewById<TextView>(R.id.calcular)
+
+        var miBundle: Bundle? =this.intent.extras
+
+        if (miBundle != null){
+            campomensaje.text="hola señor ${miBundle.getString("nombre")} "
+        }
+
+
+        }
+    private fun onClick(){
+        finish()
     }
 }
--- app/src/main/java/com/example/notasestudiante/MainActivity.kt
+++ app/src/main/java/com/example/notasestudiante/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.notasestudiante
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    var campoTexto: EditText? = null
+    var campoMateria: EditText? = null
+    var camponota1: NumberFormat? = null
+    var camponota2: NumberFormat? = null
+    var camponota3: NumberFormat? = null
+    var Calcular: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        campoTexto=findViewById(R.id.PersonName)
+        Calcular=findViewById(R.id.calcular)
+
+        val Boton:Button= findViewById(R.id.calcular)
+        Boton.setOnClickListener{onClick()}
+
+
+    }
+
+    private fun onClick() {
+        var nombre: String= campoTexto!! .text.toString()
+        Calcular!!.text="su nombre es $nombre"
+        Toast.makeText( this, "su nombre es: $nombre", Toast.LENGTH_SHORT).show()
     }
 }
